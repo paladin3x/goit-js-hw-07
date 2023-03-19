1e18c04feeb6d8fa085880eb79b14ad2e2bf9f2f
Roman Babik <kazantip2020rb@gmail.com>
2023-03-20 01:16:35 +0200

    finish hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const myGalleryList = document.querySelector(".gallery");
+myGalleryList.innerHTML = createDOMhtmlGallery(galleryItems);
+
+function createDOMhtmlGallery(items) {
+    return items.map(({ preview, original, description }) =>  
+    `<li class="gallery__item">
+        <a class="gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src=${preview}
+            alt=${description}
+            />
+        </a>
+    </li>`).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.3,
+    captionSelector: "img",
+    captionType: "attr",
+    captionDelay: 250,
+    captionsData: "alt",
+    captionClass: 'captionstyle'
+});
+ 
